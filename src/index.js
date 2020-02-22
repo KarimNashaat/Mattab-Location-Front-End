8711a739ea158303acbff6d03fed541ac19a36d2
KarimNashaat <karimnashaat97@yahoo.com>
2020-02-22 05:26:51 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -21,7 +21,12 @@ const rootReducer = combineReducers({
     record: recordingReducer
 })
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+let composeEnhancers = null;
+if (process.env.NODE_ENV === 'development') {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+} else {
+    composeEnhancers = compose;
+}
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = <Provider store={store}><BrowserRouter> <App /> </BrowserRouter> </Provider>
